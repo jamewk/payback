bdfeb501f0fdc97a41c20b8a1fe061c305ccf053
Anuwat Rodmoung <jamewk01@gmail.com>
2022-01-24 23:46:52 +0700

    Fixed delay directions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -94,32 +94,35 @@ export class AppComponent implements OnInit  {
               position: new google.maps.LatLng(value[1], value[2]),
               map: this.map,
               icon: {
-                url: "../assets/images/car.png",
-                scaledSize: new google.maps.Size(20, 40),
-                anchor: new google.maps.Point(15, 30),
-                rotation: i+1 == listPois.length?
-                this.getBearingBetweenTwoPoints(new google.maps.LatLng(listPois[i-1][1], listPois[i-1][2]), new google.maps.LatLng(value[1], value[2])): 
-                this.getBearingBetweenTwoPoints(new google.maps.LatLng(value[1], value[2]), new google.maps.LatLng(listPois[i+1][1], listPois[i+1][2])),
+                path: "M17.402,0H5.643C2.526,0,0,3.467,0,6.584v34.804c0,3.116,2.526,5.644,5.643,5.644h11.759c3.116,0,5.644-2.527,5.644-5.644 V6.584C23.044,3.467,20.518,0,17.402,0z M22.057,14.188v11.665l-2.729,0.351v-4.806L22.057,14.188z M20.625,10.773 c-1.016,3.9-2.219,8.51-2.219,8.51H4.638l-2.222-8.51C2.417,10.773,11.3,7.755,20.625,10.773z M3.748,21.713v4.492l-2.73-0.349 V14.502L3.748,21.713z M1.018,37.938V27.579l2.73,0.343v8.196L1.018,37.938z M2.575,40.882l2.218-3.336h13.771l2.219,3.336H2.575z M19.328,35.805v-7.872l2.729-0.355v10.048L19.328,35.805z",
+                scale: .7,
+                strokeColor: 'white',
+                strokeWeight: .10,
+                fillOpacity: 1,
+                fillColor: '#404040',
+                anchor: new google.maps.Point(10, 30),
+                rotation: i+1 == listPois.length?this.getBearingBetweenTwoPoints(new google.maps.LatLng(listPois[i-1][1], listPois[i-1][2]), new google.maps.LatLng(value[1], value[2])): this.getBearingBetweenTwoPoints(new google.maps.LatLng(value[1], value[2]), new google.maps.LatLng(listPois[i+1][1], listPois[i+1][2])),
               },
             });
 
             this.markers.push(marker);
             this.setMapOnAll(this.map);
-          }
-        }));
-      
-        await Promise.all(directions.map(async (value, i)=>{
-            if(i == mapSetting.order){
-              var startPoint = new google.maps.LatLng(value['departLat'], value['departLng']);
-              var endPoint = new google.maps.LatLng(value['arriveeLat'], value['arriveeLng']);
+
+            if(marker){
+              
+            }
+
+            setTimeout(() => {
+              var startPoint = new google.maps.LatLng(directions[i]['departLat'], directions[i]['departLng']);
+              var endPoint = new google.maps.LatLng(directions[i]['arriveeLat'], directions[i]['arriveeLng']);
               var directionsDisplay = new google.maps.DirectionsRenderer({
                   map: this.map,
                   preserveViewport: true,
                   suppressMarkers: true,
                   polylineOptions: {
-                      strokeColor: 'blue',
-                      strokeOpacity: 0.5,
-                      strokeWeight: 5
+                      strokeColor: 'green',
+                      strokeOpacity: 1,
+                      strokeWeight: 4
                   }
               });
       
@@ -129,14 +132,45 @@ export class AppComponent implements OnInit  {
                   travelMode: google.maps.TravelMode.DRIVING,
               }, function(response, status) {
                   if (status === 'OK') {
-
+  
                       directionsDisplay.setDirections(response);
                   } else {
                       console.log('Impossible d afficher la route ' + status)
                   }
               });
-            }
-        }))
+            }, 1000);
+          }
+        }));
+      
+        // await Promise.all(directions.map(async (value, i)=>{
+        //     if(i == mapSetting.order){
+        //       var startPoint = new google.maps.LatLng(value['departLat'], value['departLng']);
+        //       var endPoint = new google.maps.LatLng(value['arriveeLat'], value['arriveeLng']);
+        //       var directionsDisplay = new google.maps.DirectionsRenderer({
+        //           map: this.map,
+        //           preserveViewport: true,
+        //           suppressMarkers: true,
+        //           polylineOptions: {
+        //               strokeColor: 'green',
+        //               strokeOpacity: 1,
+        //               strokeWeight: 4
+        //           }
+        //       });
+      
+        //       directionsService.route({
+        //           origin: startPoint,
+        //           destination: endPoint,
+        //           travelMode: google.maps.TravelMode.DRIVING,
+        //       }, function(response, status) {
+        //           if (status === 'OK') {
+
+        //               directionsDisplay.setDirections(response);
+        //           } else {
+        //               console.log('Impossible d afficher la route ' + status)
+        //           }
+        //       });
+        //     }
+        // }))
     }
   }
 
